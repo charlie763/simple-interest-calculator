f0d7be43fec82004ae1d881ebf6763c6e6edf413
charlie763 <cwisoff@gmail.com>
2020-10-12 16:01:35 -0600

    add redux, create store, and stub out initial reducer

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-reduct'
+import { createStore } from 'redux'
+import App from './App'
+import interestReducer from './reducers/interestReducer'
 
+let store = createStore(interestReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ src/reducers/interestReducer.js
@@ -0,0 +1,10 @@
+function interestReducer(state = {}, action){
+  switch(action.type){
+    case 'CALCULATE_STATE':
+      return state
+    default:
+      return state
+  }
+}
+
+export default interestReducer
